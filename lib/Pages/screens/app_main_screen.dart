18aba98be2287bd24ca9c49c4e050268cf7e3ca5
Rakib <mdrakibr883@gmail.com>
2026-07-06 16:29:50 +0600

    completed home screen

--- lib/Pages/screens/app_main_screen.dart
+++ lib/Pages/screens/app_main_screen.dart
@@ -14,73 +14,109 @@ class AppMainScreen extends StatefulWidget {
 }
 
 class _AppMainScreenState extends State<AppMainScreen> {
-int currentIndex=0;
-  final List<Widget> _pages = [
-    FoodAppHomeScreen(),
-    Scaffold(),
-    ProfileScreen(),
-    Scaffold()
+  int currentIndex = 0;
 
-    ];
+  final List<Widget> _pages = [
+    const FoodAppHomeScreen(),
+    const Scaffold(),
+    const ProfileScreen(),
+    const Scaffold(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[currentIndex],
-      bottomNavigationBar: Container(
-        height: 90,
-        decoration: BoxDecoration(color: Colors.white),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            _buildNavItems(Iconsax.home_15, 'A', 0),
-            SizedBox(width: 10),
-            _buildNavItems(Iconsax.heart, 'B', 1),
-            SizedBox(width: 10),
 
-            _buildNavItems(Icons.person_outline, 'C', 2),
-            SizedBox(width: 10),
-
-            Stack(
-              clipBehavior: Clip.none,
+      bottomNavigationBar: Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.topCenter,
+        children: [
+          Container(
+            height: 85,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Row(
               children: [
-                _buildNavItems(Iconsax.shopping_cart, 'D', 3),
-                Positioned(
-                  top: 16,
-                  right: -7,
-                  //we will make this cart item dynamic letter
-                  child: CircleAvatar(
-                    backgroundColor: red,
-                    radius: 10,
-                    child: Text(
+                Expanded(
+                  child: _buildNavItems(Iconsax.home_15, 0),
+                ),
+                Expanded(
+                  child: _buildNavItems(Iconsax.heart, 1),
+                ),
 
-                      "0",
-                      style: TextStyle(fontSize: 12, color: Colors.white),
-                    ),
+                /// Space for Search Button
+                const SizedBox(width: 70),
+
+                Expanded(
+                  child: _buildNavItems(Icons.person_outline, 2),
+                ),
+
+                Expanded(
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    alignment: Alignment.center,
+                    children: [
+                      _buildNavItems(Iconsax.shopping_cart, 3),
+
+                      Positioned(
+                        top: 10,
+                        right: 18,
+                        child: CircleAvatar(
+                          radius: 9,
+                          backgroundColor: red,
+                          child: const Text(
+                            "0",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 11,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Positioned(
-                    right: 155,
-                    top:-25,
-                    child: CircleAvatar(backgroundColor: red,
-
-                  radius: 35,
-                 child: Icon(
-                   CupertinoIcons.search,
-                   size: 35,
-                   color: Colors.white,
-                 )
-                ))
               ],
             ),
-          ],
-        ),
+          ),
+
+          /// Floating Search Button
+          Positioned(
+            top: -28,
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  color: red,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: red.withOpacity(.35),
+                      blurRadius: 15,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: const Icon(
+                  CupertinoIcons.search,
+                  color: Colors.white,
+                  size: 34,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget _buildNavItems(IconData icon, String label, int index) {
-    // int currentIndex = 0;
+  Widget _buildNavItems(IconData icon, int index) {
+    final bool isSelected = currentIndex == index;
+
     return InkWell(
       onTap: () {
         setState(() {
@@ -93,14 +129,20 @@ int currentIndex=0;
           Icon(
             icon,
             size: 28,
-            color: currentIndex == index ? red : Colors.grey,
-          ),
-          SizedBox(height: 3),
-          CircleAvatar(
-            radius: 3,
-            backgroundColor: currentIndex == index ? red : Colors.transparent,
+            color: isSelected ? red : Colors.grey,
           ),
 
+          const SizedBox(height: 4),
+
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 250),
+            width: 6,
+            height: 6,
+            decoration: BoxDecoration(
+              color: isSelected ? red : Colors.transparent,
+              shape: BoxShape.circle,
+            ),
+          ),
         ],
       ),
     );
--- lib/widgets/prouducst_item_display.dart
+++ lib/widgets/prouducst_item_display.dart
@@ -4,91 +4,120 @@ import 'package:food_delivery/models/product_model.dart';
 class ProuducstItemDisplay extends StatefulWidget {
   final FoodModel foodModel;
 
-  const ProuducstItemDisplay({super.key, required this.foodModel});
+  const ProuducstItemDisplay({
+    super.key,
+    required this.foodModel,
+  });
 
   @override
-  State<ProuducstItemDisplay> createState() => _ProuducstItemDisplayState();
+  State<ProuducstItemDisplay> createState() =>
+      _ProuducstItemDisplayState();
 }
 
 class _ProuducstItemDisplayState extends State<ProuducstItemDisplay> {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+    final size = MediaQuery.of(context).size;
+
     return GestureDetector(
       onTap: () {},
-
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-
-        children: [
-          Positioned(
-            bottom: 35,
-            child: Container(
-              height: 180,
-              width: size.width * 0.5,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withAlpha(10),
-                    spreadRadius: 10,
-                    blurRadius: 20,
-                  ),
-                ],
+      child: SizedBox(
+        width: size.width * .5,
+        height: 270,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Positioned(
+              bottom: 20,
+              child: Container(
+                width: size.width * .5,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withAlpha(15),
+                      blurRadius: 20,
+                      spreadRadius: 5,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Container(
-            width: size.width * 0.5,
-            padding: EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ClipOval(
-                  child: Image.network(
-                    widget.foodModel.imageCard,
-                    width: 120,
-                    height: 120,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  child: Text(
-                    widget.foodModel.name,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 35),
+              child: SizedBox(
+                width: size.width * .5,
+                child: Column(
+                  children: [
+                    ClipOval(
+                      child: Image.network(
+                        widget.foodModel.imageCard,
+                        width: 105,
+                        height: 105,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                ),
-                Text(
-                  widget.foodModel.specialItems,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 14,
-                    height: 1.2,
-                  ),
-                ),
-                const Spacer(),
-                RichText(
-                  text: TextSpan(
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+
+                    const SizedBox(height: 10),
+
+                    SizedBox(
+                      height: 44,
+                      child: Text(
+                        widget.foodModel.name,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
-                    children: [TextSpan(text: "\$${widget.foodModel.price}")],
-                  ),
+
+                    const SizedBox(height: 5),
+
+                    Text(
+                      widget.foodModel.specialItems,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+
+                    const Spacer(),
+
+                    Text(
+                      "\$${widget.foodModel.price}",
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+
+            Positioned(
+              top: 10,
+              right: 10,
+              child: CircleAvatar(
+                radius: 16,
+                backgroundColor: Colors.red.shade100,
+                child: Image.asset(
+                  "assets/food-delivery/icon/fire.png",
+                  width: 18,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
